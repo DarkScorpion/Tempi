c82ade8b6f44c01107fdd420991d6e6e0563ecee
Alex Smith <iqwz@ya.ru>
2015-07-25 21:13:05 +0900

    init commit

---
+++ Tempi/Tempi.php
@@ -0,0 +1,35 @@
+<?
+class Tempi
+{
+  private $_leftBound;
+  private $_rightBound;
+
+  public function __construct ($leftBound = '##', $rightBound = null)
+  {
+    if($rightBound)
+    {
+      $this->_leftBound = $leftBound;
+      $this->_rightBound = $rightBound;
+    } else {
+      $this->_leftBound = $leftBound;
+      $this->_rightBound = $leftBound;
+    }
+  }
+
+  public function replace($html, $dataArr)
+  {
+    if(is_array($dataArr) || is_object($dataArr))
+    {
+      $lb = $this->_leftBound;
+      $rb = $this->_rightBound;
+      foreach ($dataArr as $key => $value)
+      {
+        $pattern = "$lb$key$rb";
+        $html = str_replace($pattern, $value, $html);
+      }
+      return $html;
+    } else {
+      throw new Exception('Tempi.replace param: Must be array or object');
+    }
+  }
+}
